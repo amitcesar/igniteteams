8313b63eb699de1e55aac5b878ea1581fdc337e4
Amit Cesar <amitcesar27@gmail.com>
2022-10-01 14:32:04 -0300

    fix: add types navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Button } from "@components/Button";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
 
-import { useState } from "react";
-import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  function handleNewGroup() {}
 
   return (
     <Container>
@@ -28,7 +31,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar Nova Turma" />
+      <Button title="Criar Nova Turma" onPress={handleNewGroup} />
     </Container>
   );
 }
