8b19fb9bb523f7edf4fbb2f83226ef0efcdbbb1b
Amit Cesar <amitcesar27@gmail.com>
2022-10-04 19:19:01 -0300

    feat:custom Erros and handle exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+
+import { AppError } from "@utils/AppError";
+import { createGroup } from "@storage/group/groupCreate";
 
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -6,8 +10,7 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 import { Container, Content, Icon } from "./styles";
-import { useState } from "react";
-import { createGroup } from "@storage/group/groupCreate";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
@@ -15,9 +18,19 @@ export function NewGroup() {
 
   async function handleNavigationToPlayerScreen() {
     try {
+      if (group.trim().length === 0) {
+        Alert.alert("Novo Grupo", "Digite o nome da Turma");
+      }
+
       await createGroup(group);
       navigation.navigate("players", { group });
     } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Criar Novo Grupo", error.message);
+      } else {
+        Alert.alert("Criar Novo Grupo", "Não foi possivel criar um novo Grupo");
+        console.log(error);
+      }
       console.log(error);
     }
   }
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,11 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { groupsGetAll } from "./groupGetAll";
 
 import {GROUP_COLLECTION} from '@storage/storageConfig'
-import { groupsGetAll } from "./groupGetAll";
+import { AppError } from "@utils/AppError";
+
 
 export async function createGroup(NewGroup: string) {
     try {
       const storedGroups = await groupsGetAll();
+      const groupAlreadyExist = storedGroups.includes(NewGroup)
+
+        if(groupAlreadyExist){
+          throw new AppError('Já existe um grupo com esse nome.');
+        }
+
       const storage = JSON.stringify([...storedGroups, NewGroup])
       await AsyncStorage.setItem(GROUP_COLLECTION, storage);
       
